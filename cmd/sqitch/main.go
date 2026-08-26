0d90a8571e315d7dc785de090fadb5ee5a6df75a
Mark Grimes <mgrimes@tgfmail.org>
2026-08-26 16:37:27 -0400

    chore: update version to 1.0.1

--- cmd/sqitch/main.go
+++ cmd/sqitch/main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/sqitchers/sqitch-go/internal/command"
 )
 
-var version = "0.1.7"
+var version = "1.0.1"
 
 func main() {
 	versionInfo := fmt.Sprintf("gsqitch (golang) v%s", strings.TrimPrefix(version, "v"))
